b72bc8f690d38fd8520d1e2cf49f939dd49b7bc1
Pujan Khunt <pujankhunt2412@gmail.com>
2025-10-15 05:55:30 +0530

    strict bracket placement for creating blocks in go

---
+++ go-101/lineBreaks.go
@@ -0,0 +1,13 @@
+package main
+
+// This kind of brackets placement would give a syntax error.
+// This code wouldn't compile.
+// This kind of placement for brackets would result error in for loops, if else and in
+// other situations too.
+// func main()
+// {
+//
+// }
+
+func main() {
+}
